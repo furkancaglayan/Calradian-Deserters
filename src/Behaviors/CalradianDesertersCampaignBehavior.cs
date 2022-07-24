f42dda2251b9dfde51345194403ff480d8280daf
Furkan Çağlayan <furkancaglayan15@hotmail.com>
2022-07-24 17:32:11 +0300

    updates.

--- src/Behaviors/CalradianDesertersCampaignBehavior.cs
+++ src/Behaviors/CalradianDesertersCampaignBehavior.cs
@@ -1,32 +1,69 @@
-﻿using System;
+﻿using CalradianDeserters.Components;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TaleWorlds.CampaignSystem;
+using TaleWorlds.CampaignSystem.Actions;
 using TaleWorlds.CampaignSystem.MapEvents;
+using TaleWorlds.CampaignSystem.Party;
+using TaleWorlds.Core;
 
 namespace CalradianDeserters.Behaviors
 {
     public class CalradianDesertersCampaignBehavior : CampaignBehaviorBase
     {
+        private Clan Deserters => Clan.FindFirst(x => x.StringId == "deserters");
         public override void RegisterEvents()
         {
             CampaignEvents.MapEventEnded.AddNonSerializedListener(this, OnMapEventEnded);
+            CampaignEvents.MobilePartyCreated.AddNonSerializedListener(this, MobilePartyCreated);
             CampaignEvents.OnNewGameCreatedPartialFollowUpEndEvent.AddNonSerializedListener(this, OnNewGameCreated);
+        }
+
+        private void MobilePartyCreated(MobileParty mobileParty)
+        {
+            if (mobileParty.ActualClan == Deserters && !IsDeserter(mobileParty))
+            {
+                DestroyPartyAction.Apply(null, mobileParty);
+            }
         }
 
         private void OnNewGameCreated(CampaignGameStarter campaignGameStarter)
         {
+            foreach (var mobileParty in MobileParty.All.ToList())
+            {
+                if (mobileParty.ActualClan == Deserters && !IsDeserter(mobileParty))
+                {
+                    DestroyPartyAction.Apply(null, mobileParty);
+                }
+            }
         }
 
         private void OnMapEventEnded(MapEvent mapEvent)
         {
+            if (mapEvent.HasWinner)
+            {
+                var defeatedLeader = mapEvent.GetLeaderParty(mapEvent.DefeatedSide);
+                var winningLeader = mapEvent.GetLeaderParty(mapEvent.WinningSide);
+
+                if (defeatedLeader.IsMobile && winningLeader.IsMobile && defeatedLeader.MobileParty.Army != null && winningLeader.MobileParty.Army != null)
+                {
+                    var troops = defeatedLeader.MapEventSide.Parties.First().Troops;
+                    var casualties = defeatedLeader.MapEventSide.Casualties;
+                }
+            }
         }
 
         public override void SyncData(IDataStore dataStore)
         {
 
+        }
+
+        private bool IsDeserter(MobileParty mobileParty)
+        {
+            return mobileParty.PartyComponent is DeserterPartyComponent;
         }
     }
 }
